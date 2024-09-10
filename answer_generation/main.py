1ba55d0b3fd6e9ecba1cd59120b89adfbb873d44
MakArtKar <makoyan2001@gmail.com>
2024-09-10 18:30:52 +0200

    added refine

--- answer_generation/main.py
+++ answer_generation/main.py
@@ -1,12 +1,18 @@
 import json
+import logging
 import os
 from urllib.parse import urljoin
 
 import httpx
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain.chains import RefineDocumentsChain, LLMChain
+from langchain.docstore.document import Document
+
+from prompts import prompt_template, refine_prompt_template
+
+logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 
@@ -16,22 +22,27 @@ app = FastAPI()
 llm = ChatOpenAI(model='gpt-4o-mini')
 
 
-prompt = PromptTemplate.from_template(
-    """Ты эксперт по строительной документации. Я задам тебе вопрос, а также дам релевантную информацию из документации. """
-    """Ты должен ответить на поставленный вопрос на русском языке, используя только информацию из контекста, НИЧЕГО не добавляй от себя. """
-    """Вопрос: {question}\nКонтекст: {context}\n"""
-)
-
-
-def answer_question(query: str, docs: list[str]):
-    chain = prompt | llm
-    context = '\n'.join(docs)
-    result = chain.invoke({'question': query, 'context': context})
-    return result
+def answer_question(query: str, docs: list[str], approach='concat'):
+    if approach == 'concat':
+        chain = prompt_template | llm
+        context = '\n'.join(docs)
+        result = chain.invoke({'question': query, 'context': context})
+        return result
+    elif approach == 'refine':
+        docs = [Document(page_content=doc) for doc in docs]
+        chain = RefineDocumentsChain(
+            initial_llm_chain=LLMChain(prompt=prompt_template, llm=llm),
+    refine_llm_chain=LLMChain(prompt=refine_prompt_template, llm=llm),
+            document_variable_name="context",
+            initial_response_name="existing_answer",
+        )
+        return chain.run(input_documents=docs, question=query)
+    else:
+        raise ValueError(f"In function `answer_question` param `approach` can't be {approach}")
 
 
 @app.get("/ask")
-async def ask_question(query: str):
+async def ask_question(query: str, approach='concat'):
     async with httpx.AsyncClient() as client:
         try:
             query_params = {"query": query, "num": 5}
@@ -39,6 +50,6 @@ async def ask_question(query: str):
             if response.status_code != 200:
                 raise HTTPException(status_code=response.status_code, detail=response.content.decode())
             docs = json.loads(response.content)['results']['documents']
-            return {"response": answer_question(query, docs[0])}
+            return {"response": answer_question(query, docs[0], approach=approach)}
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
